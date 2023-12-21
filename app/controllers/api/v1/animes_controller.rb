b75ae930600e2d0444cb2f701feee1a7d8edb7e4
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-21 16:36:46 -0300

    23° commit - tdd controller: Consegue criar um anime e retornar status 201? GREEN

--- app/controllers/api/v1/animes_controller.rb
+++ app/controllers/api/v1/animes_controller.rb
@@ -11,6 +11,15 @@ class Api::V1::AnimesController < ApplicationController
     render json: @anime
   end
 
+  def create
+    @anime = Anime.new(anime_params)
+    if @anime.save
+      render json: @anime, status: :created, location: api_v1_anime_url(@anime) 
+    else
+      render json: @anime.errors, status: :unprocessable_entity
+    end
+  end
+
 private
 
 def set_anime
