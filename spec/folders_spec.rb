8f02812a4070342a05b96f176887a7602c1e8d57
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-21 15:42:05 -0300

    4° commit - tdd folders: pasta spec/controllers existe? RED

--- spec/folders_spec.rb
+++ spec/folders_spec.rb
@@ -1,7 +1,11 @@
-RSpec.describe 'Teste de verificação de existencia de pastas' do
+RSpec.describe 'Testes de verificação de existencia de pastas' do
 
   it 'pasta spec/models existe?' do
     expect(Dir.exist?('spec/models')).to eq(true)
+  end
+
+  it 'pasta spec/controllers existe?' do
+    expect(Dir.exist?('spec/controllers')).to eq(true)
   end
 
 end
