246bf58c5dd62b3b2ea6f48cc328d57c92feaed2
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-21 16:46:55 -0300

    27° commit - tdd controller: Consegue excluir um anime e retornar status 204? GREEN

--- app/controllers/api/v1/animes_controller.rb
+++ app/controllers/api/v1/animes_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::AnimesController < ApplicationController
 
-  before_action :set_anime, only: %i[show update] # show update destroy
+  before_action :set_anime, only: %i[show update destroy] # show update destroy
 
   def index
     @animes = Anime.all 
@@ -26,6 +26,10 @@ class Api::V1::AnimesController < ApplicationController
     else
       render json: @anime.errors, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @anime.destroy!
   end
 
 private
