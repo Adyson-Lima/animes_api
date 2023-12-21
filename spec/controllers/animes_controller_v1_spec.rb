1887c9a38a9b3b0c1937aac53fb924f52ce625e0
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-21 16:29:11 -0300

    20° commit - tdd controller: Consegue listar um anime especifico e retornar status 200? RED

--- spec/controllers/animes_controller_v1_spec.rb
+++ spec/controllers/animes_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::AnimesController, type: :controller do
     end
   end
 
+  describe 'GET /api/v1/animes/id' do
+    it 'Consegue listar um anime especifico e retornar status 200?' do
+      get :show, params: {id: @anime.id}
+      expect(response.body).to include_json(name: 'fly')
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
